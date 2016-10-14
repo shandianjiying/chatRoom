b55c4b8db91266ae88295988cc1cfc219163d940
军 单 <shandianjiying@126.com>
2016-10-14 16:54:31 +0800

    新增sequelize和mysql模块

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var users = require('./routes/users');
 
 var app = express();
 
-// view engine setup
+// view engine setupn
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
